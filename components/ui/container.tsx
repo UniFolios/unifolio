38b6ee5c8a27351946cf0f45a0d08ffd304753ae
Shovan Das <shovondas26@gmail.com>
2024-11-13 20:07:31 +0200

    initial structure

---
+++ components/ui/container.tsx
@@ -0,0 +1,15 @@
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container = ({
+    children,
+}: ContainerProps) => {
+    return ( 
+        <div className="container mx-auto px-4">
+            {children}
+        </div>
+     );
+}
+
+export default Container;
