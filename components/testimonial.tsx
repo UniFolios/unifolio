38f227d7ce1725ec934eb2b853dabd02a5983a2f
Shovan Das <shovondas26@gmail.com>
2024-11-20 00:40:47 +0200

    testimonial section

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -1,9 +1,27 @@
+import Image from "next/image";
+import IconChat from "./icons/chat";
+import Section from "./ui/section";
+import SectionTitle from "./ui/section-title";
+import IconLinkedIn from "./icons/linkedin";
+
 const Testimonial = () => {
-    return ( 
-        <div>
-            Testimonial
-        </div>
-     );
+    return (
+        <Section>
+            <SectionTitle title="Lucky encounters" icon={<IconChat />} />
+            <div className="flex flex-row items-stretch justify-center gap-8 py-8 bg-gray-50 w-2/3">
+                <Image src="/img/testimonial-1.png" width={191} height={250} alt="testimonial 1" />
+                <div>
+                    <div className="flex gap-4">
+                        <h2 className="text-2xl font-semibold pb-4 underline">Hans Landa</h2>
+                        <IconLinkedIn />
+                    </div>
+                    <h3 className="text-xl font-normal pb-8">Product Manager at Technocorp</h3>
+                    <p>Jon consistently went above and beyond to ensure project success. His innovative ideas and attention to detail were invaluable to our team. </p>
+                </div>
+            </div>
+            
+        </Section>
+    );
 }
- 
+
 export default Testimonial;
