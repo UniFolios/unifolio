825ee6bebd4e95407b2db3a75cb9becec067117d
Shovan Das <shovondas26@gmail.com>
2024-12-13 10:08:39 +0200

    refactored and formatted

--- components/banner.tsx
+++ components/banner.tsx
@@ -15,7 +15,7 @@ const Banner = () => {
             sm:w-[720px] sm:px-[24px] sm:items-center sm:pb-4
             xsm:w-[320px] xsm:items-end xsm:pb-4
             ">
-                
+
                 <div className="z-10 absolute 
                 flex flex-col 
                 xl:top-1/3 xl:left-[0px] xl:gap-10
@@ -23,7 +23,7 @@ const Banner = () => {
                 md:top-1/3 md:left-[0px] md:gap-6                
                 sm:top-1/3 sm:left-[60px] sm:gap-2
                 xsm:top-32 xsm:left-0 xsm:gap-2 xsm:pb-4    
-                "> 
+                ">
                     <Fade direction="up" duration={1200} triggerOnce>
                         <h1 className="font-italiana text-slate-900
                         xl:text-8xl
@@ -42,7 +42,7 @@ const Banner = () => {
                         sm:text-base
                         xsm:text-sm
                         ">Full Stack Developer</p>
-                    </Fade>                   
+                    </Fade>
                 </div>
 
                 <div className="relative bg-slate-200 
@@ -57,15 +57,15 @@ const Banner = () => {
                     sm:w-[250px] sm:h-[350px] sm:right-10
                     xsm:w-[320px] xsm:h-[450px] xsm:right-0
                     ">
-                    <Image
-                        src="/profile-pic.png"
-                        alt="banner"
-                        width={350}
-                        height={450}
-                        className="object-fit absolute bottom-0 right-0"
-                    /> 
+                        <Image
+                            src="/profile-pic.png"
+                            alt="banner"
+                            width={350}
+                            height={450}
+                            className="object-fit absolute bottom-0 right-0"
+                        />
                     </div>
-                    
+
 
                     <div className="bg-white absolute bottom-0 left-0
                     xl:w-[110px] xl:h-[120px]
@@ -73,12 +73,12 @@ const Banner = () => {
                     md:w-[90px] md:h-[100px]
                     sm:w-[80px] sm:h-[90px] 
                     xsm:w-[70px] xsm:h-[80px] 
-                    ">                    
-                    <a 
+                    ">
+                        <a
 
-                    href="/resume.pdf"
-                    download="Hugo_Stiglitz_CV"
-                    className="absolute bottom-0 left-0 bg-slate-700 hover:bg-slate-600 flex items-center justify-center
+                            href="/resume.pdf"
+                            download="Hugo_Stiglitz_CV"
+                            className="absolute bottom-0 left-0 bg-slate-700 hover:bg-slate-600 flex items-center justify-center
                     xl:w-[95px] xl:h-[105px]
                     lg:w-[85px] lg:h-[95px]
                     md:w-[80px] md:h-[90px]  
@@ -92,29 +92,11 @@ const Banner = () => {
                                 height={32}
                                 className="object-contain"
                             />
-                    </a>
-                           
-                    </div>
-                           
-                    
-                   
-                    {/* <AttentionSeeker effect="flash" delay={1000}>
-                        <a
-                            href="/resume.pdf"
-                            download="Hugo_Stiglitz_CV"
-                            className="absolute bottom-0 left-0 pt-4 pr-4 ps-0 pb-0 bg-black transition-all duration-300 hover:pt-5 hover:pr-5"
-                        >
-                            <Image
-                                src="/cv.svg"
-                                alt="Download Resume"
-                                width={80}
-                                height={80}
-                                className="object-contain"
-                            />
                         </a>
-                    </AttentionSeeker> */}
+
+                    </div>
                 </div>
-                
+
             </div>
         </Section>
     );
